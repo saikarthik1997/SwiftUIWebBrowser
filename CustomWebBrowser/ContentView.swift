2991341d9423f3b22021eff084439eeaff558562
Sai Karthik <sai@retire100.live>
2024-06-06 23:49:59 +0530

    webview code

--- CustomWebBrowser/ContentView.swift
+++ CustomWebBrowser/ContentView.swift
@@ -1,24 +1,82 @@
-//
-//  ContentView.swift
-//  CustomWebBrowser
-//
-//  Created by Sai Sistla on 06/06/24.
-//
-
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    let webView = WebView()
+    
+    @State var urlString = "https://www.google.com"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button(action: {
+                    webView.goBack()
+                }){
+                    Image(systemName: "arrow.backward")
+                        .font(.title)
+                        .padding()
+                }
+                
+                TextField("Enter url", text: $urlString)
+                
+                Button(action: {
+                     webView.loadURL(urlString: urlString)
+                }, label: {
+                    Text("Go")
+                    
+                })
+
+                Button(action: {
+                     webView.goForward()
+                }){
+                    Image(systemName: "arrow.forward")
+                        .font(.title)
+                        .padding()
+                    
+                    
+                }
+            }.background(Color(.systemGray6))
+            
+            // main webview
+            webView
         }
-        .padding()
+        .onAppear(){
+             webView.loadURL(urlString: urlString)
+        }
     }
 }
-
 #Preview {
     ContentView()
+}
+
+struct WebView: UIViewRepresentable {
+    
+    let webView: WKWebView
+    
+    init() {
+        self.webView = WKWebView()
+        
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        webView.allowsBackForwardNavigationGestures = true
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+    
+    func goBack(){
+        webView.goBack()
+    }
+    
+    func goForward(){
+        webView.goForward()
+    }
+    
+    
+    func loadURL(urlString: String) {
+        webView.load(URLRequest(url: URL(string: urlString)!))
+    }
 }
