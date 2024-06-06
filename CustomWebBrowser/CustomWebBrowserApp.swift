40400d5466e8915bfa8619c29b2a45714f2d43f3
Sai Karthik <sai@retire100.live>
2024-06-06 23:28:53 +0530

    Initial Commit

---
+++ CustomWebBrowser/CustomWebBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomWebBrowserApp.swift
+//  CustomWebBrowser
+//
+//  Created by Sai Sistla on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomWebBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
